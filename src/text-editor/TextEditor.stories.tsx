5ddf949693093e30f3a2fb45d9e9f66f43a7362e
Aaron Mathews <aamathews23@gmail.com>
2021-09-07 13:22:35 -0700

    Created a basic text editor

---
+++ src/text-editor/TextEditor.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+// Import Component
+import { TextEditor, TextEditorProps } from './TextEditor';
+
+export default {
+  component: TextEditor,
+  title: 'Features/TextEditor',
+} as Meta;
+
+const Template: Story<TextEditorProps> = (args) => <TextEditor {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    "id": "default",
+};
---
+++ src/text-editor/TextEditor.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./TextEditor.css";
+
+// Import Components
+import { Bold } from "../bold/Bold";
+import { Button } from "../button/Button";
+import { Italic } from "../italic/Italic";
+import { MarkdownInput } from "../markdown-input/MarkdownInput";
+import { MarkdownOutput } from "../markdown-output/MarkdownOutput";
+import { Preview } from "../preview/Preview";
+
+export interface TextEditorProps {
+    id: string;
+}
+
+export const TextEditor = (props: TextEditorProps) => {
+    const [markdown, setMarkdown] = useState<string>("");
+    const [showPreview, setShowPreview] = useState<boolean>(false);
+
+    return (
+        <div
+            id={ props.id }
+            data-testid={ props.id }
+        >
+            <div>
+                <Bold
+                    id="bold-btn"
+                />
+                <Italic
+                    id="italic-btn"
+                />
+
+                <Preview
+                    id="preview-btn"
+                    showPreview={ showPreview }
+                    callback={ (newShowPreview) => setShowPreview(!newShowPreview) }
+                />
+            </div>
+
+            <div>
+                {
+                    !showPreview &&
+                    <MarkdownInput
+                        id="markdown-input"
+                        placeholder="Enter markdown here..."
+                        value={ markdown }
+                        callback={ (newMarkdown) => setMarkdown(newMarkdown) }
+                    />
+                }
+
+                {
+                    showPreview &&
+                    <MarkdownOutput
+                        id="markdown-output"
+                        text={ markdown }
+                    />
+                }
+            </div>
+
+            <div>
+                <Button
+                    id="save-btn"
+                    label="Save"
+                />
+
+                <Button
+                    id="reset-btn"
+                    label="Reset"
+                    outline
+                />
+            </div>
+        </div>
+    );
+};
