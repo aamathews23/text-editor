1ffc4f870a0e1796043bf7d3cc5033fa33fe7ece
Aaron Mathews <aamathews23@gmail.com>
2021-09-09 13:41:01 -0700

    Refactored teh button component to use styled components

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,5 +1,25 @@
 import React from "react";
-import "./Button.css"
+import styled from "styled-components";
+
+interface StyledButtonProps {
+    outline: boolean;
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
+    font-size: 14px;
+    padding: 0.5rem 1rem;
+    border-radius: 1rem;
+    font-family: 'Open Sans', sans-serif;
+    border: 1px solid #424242;
+    color: ${ props => props.outline ? "#424242" : "white" };
+    background-color: ${ props => props.outline ? "white" : "#424242" };
+    &:hover {
+        border-color: ${ props => props.outline ? "#424242" : "#0f0f0f" };
+        background-color: ${ props => props.outline ? "#424242" : "#0f0f0f" };
+        color: white;
+        cursor: pointer;
+    }
+`;
 
 export interface ButtonProps {
     id: string;
@@ -10,13 +30,13 @@ export interface ButtonProps {
 
 export const Button = (props: ButtonProps) => {
     return (
-        <button
+        <StyledButton
             id={ props.id }
             data-testid={ props.id }
-            className={ `button${ props.outline ? " outline" : "" }` }
+            outline={ props.outline || false }
             onClick={ props.callback }
         >
             <strong>{ props.label.toUpperCase() }</strong>
-        </button>
+        </StyledButton>
     );
 };
